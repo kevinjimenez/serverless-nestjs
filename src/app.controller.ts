6409b2e0620ada5d477b991a35d6e83025c90a5a
kevin jimenez <kivito2009@hotmail.com>
2021-08-20 09:02:05 -0500

    mi primer serverless en nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('principal')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  get(): string {
+    console.log('hahaha que');
+    return this.appService.getHello();
+  }
+
+  @Get('hello')
+  getHello(): string {
+    console.log('hahaha que');
+    return this.appService.getHello();
+  }
+
+  @Post('post')
+  postHello(@Body() datos: any): string {
+    console.log('hahaha que');
+    console.log(datos);
+    return this.appService.getHello();
+  }
+}
